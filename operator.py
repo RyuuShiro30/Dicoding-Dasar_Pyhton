97689c1e406b36f0de6f6add8e7d0cf37619161a
RyuuShiro30 <annoraegaprasetyo@gmail.com>
2025-11-02 21:08:30 +0700

    operator python

---
+++ operator.py
@@ -0,0 +1,99 @@
+x = 11
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x // y)
+print(x / y)
+print(x % y)
+print(x ** y)
+
+"""
+Output:
+
+16
+6
+55
+2
+2.2
+1
+161051
+"""
+
+x = 5
+y = 10
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+"""
+Output:
+
+False
+True
+False
+True
+False
+True
+"""
+
+x = "Dicoding"
+y = "Indonesia"
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+"""
+Output:
+
+False
+True
+False
+True
+False
+True
+"""
+
+# +=
+x = 11
+x += 5
+print(x)
+
+# -=
+x = 11
+x -= 5
+print(x)
+
+# *=
+x = 11
+x *= 5
+print(x)
+
+# /=
+x = 11
+x /= 5
+print(x)
+
+#%=
+x = 11
+x%= 5
+print(x)
+
+
+"""
+Output:
+16
+6
+55
+2.2
+1
+"""
